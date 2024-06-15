c8b9cb73adb31a860f1658faa7199535ef0f190e
alkachhirang <alkachhirang3@gmail.com>
2024-06-15 12:08:16 +0530

    change in dropdown

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -6,7 +6,7 @@ export default function Dropdown() {
         <>
             <ul className="dropdown absolute invisi ble opacity-0 h-0 border border-solid border-transparent bg-offBlack rounded p-0 top-[35px] left-[50%] mt-[2px] z-[2] translate-x-[-50%] transition-all duration-300 ease-linear">
                 {DropdownData.map((data, index) => (
-                    <li key={index} className="px-5 !text-lightWhite ff_poppins text-base font-normal p-[6px_20px] text-nowrap text-center bg-transparent "><Link href="" className="text-lightWhite">
+                    <li key={index} className="px-5 !text-lightWhite ff_poppins text-base font-normal p-[6px_20px] text-nowrap text-center bg-transparent "><Link href="" className="text-lightWhite hover:text-lightRed transition-all ease-linear duration-300">
                         {data.title}
                     </Link></li>
                 ))}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -110,5 +110,4 @@ const Header = () => {
         </div>
     );
 };
-
 export default Header;
